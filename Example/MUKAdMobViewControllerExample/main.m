85083582af12d21810dc88dde1584b86bd3f9c91
Muccy <muccymac@gmail.com>
2013-07-10 15:05:22 +0200

    First commit

---
+++ Example/MUKAdMobViewControllerExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MUKAdMobViewControllerExample
+//
+//  Created by Marco on 10/07/13.
+//  Copyright (c) 2013 MeLive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
